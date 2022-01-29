ff95f2eb0b4cb8521e79f9181c68dd12aa67c8d8
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-29 10:24:36 +0300

    Initial Commit

---
+++ TILL/TILLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILLApp.swift
+//  TILL
+//
+//  Created by Владимир Рябов on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TILLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
